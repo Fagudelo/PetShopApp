0d454279afcacabb34407e6d51d4673a76f583fb
Fagudelo <ferney.certifications@outlook.com>
2024-09-13 15:19:36 -0500

    Se crea el método para listar los productos y una card para la visualización

--- petshopapp.client/src/App.jsx
+++ petshopapp.client/src/App.jsx
@@ -1,12 +1,49 @@
 import { useEffect, useState } from 'react';
+import { Container, Row, Col, Card, CardHeader, CardBody, Button, CardTitle } from 'reactstrap';
+import ProductsComponent from './components/ProductsComponent'
 import './App.css';
 
 function App() {
 
+    const [productos, setProductos] = useState([])
+
+    const mostrarProductos = async () => {
+        const response = await fetch("api/product/List");
+
+        if (response.ok) {
+            const data = await response.json();
+            setProductos(data);
+        } else {
+            console.log("List error");
+        }
+    }
+
+    useEffect(() => {
+        mostrarProductos();
+    }, [])
+
     return (
-        <div>
-            <h1>Hello worl!</h1>
-        </div>
+        <>
+            <Container>
+                <Row>
+                    <Col sm="12">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle tag="h5">
+                                    Lista de productos
+                                </CardTitle>                                
+                            </CardHeader>
+                            <CardBody>
+                                <Button size="sm" color="success" >Nuevo Producto</Button>{' '}
+                                <hr></hr>
+                                <ProductsComponent data={ productos }>
+                                </ProductsComponent>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
 
