b4eaa6c820676a19439fedda3b2bf435ba0dac25
Fagudelo <ferney.certifications@outlook.com>
2024-09-13 14:56:25 -0500

    Se agregan los métodos para listar y agregar productos

--- PetShopApp.Server/Controllers/ProductController.cs
+++ PetShopApp.Server/Controllers/ProductController.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetShopApp.Server.Models;
 
 namespace PetShopApp.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductController(PetShopDbContext dbContext) : ControllerBase
     {
+        private readonly PetShopDbContext _dbContext = dbContext;
+
+        [HttpGet]
+        [Route("List")]
+        public async Task<IActionResult>List()
+        {
+            List<Product> list = await _dbContext.Products.OrderByDescending(p => p.Id).ToListAsync();
+
+            return StatusCode(StatusCodes.Status200OK, list);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public async Task<IActionResult> Add([FromBody] Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "Ok");
+        }
     }
 }
