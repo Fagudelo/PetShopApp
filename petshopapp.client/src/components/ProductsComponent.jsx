9e5fc65424f01a8831c041b2483195af23453358
Fagudelo <ferney.certifications@outlook.com>
2024-09-13 23:51:56 -0500

    Se configuran las propiedades 'setEditar' y 'eliminarProducto' en el archivo 'ProductsComponent.jsx'

--- petshopapp.client/src/components/ProductsComponent.jsx
+++ petshopapp.client/src/components/ProductsComponent.jsx
@@ -1,49 +1,54 @@
 import { Table, Button } from 'reactstrap';
-import PropTypes from 'prop-types';
-function ProductsComponent({ data }) {
+import propTypes from 'prop-types';
+function ProductsComponent({ data, mostrarModal, setMostrarModal, setEditar, eliminarProducto }) {
 
-    
+    const enviarDatos = (producto) => {
+        setEditar(producto)
+        setMostrarModal(!mostrarModal)
+    }
 
-  return (
-      <Table striped responsive>
-          <thead>
-              <tr>
-                  <th>Name</th>
-                  <th>Category</th>
-                  <th>Price</th>
-                  <th>Quantity in Stock</th>
-              </tr>
-          </thead>
-          <tbody>
-              {
-                  (data.length < 1) ? (
-                      <tr>
-                          <td colSpan="4">Sin registros</td>
-                      </tr>
-                  ) : (
-                      data.map((item) => (
-                          <tr key={item.id}>
-                              <td>{item.name}</td>
-                              <td>{item.category}</td>
-                              <td>{item.price}</td>
-                              <td>{item.quantityInStock}</td>
-                              <td>
-                                  <Button color="primary" size="sm" className="me-2" >Edit</Button>
-                                  <Button color="danger" size="sm" >Delete</Button>
-                              </td>
-                          </tr>
-                      ))
-                  )
-              }
-          </tbody>
-      </Table>
-  );
+    return (
+        <Table striped responsive>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                    <th>Quantity in Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    (data.length < 1) ? (
+                        <tr>
+                            <td colSpan="4">Sin registros</td>
+                        </tr>
+                    ) : (
+                        data.map((item) => (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.category}</td>
+                                <td>{item.price}</td>
+                                <td>{item.quantityInStock}</td>
+                                <td>
+                                    <Button color="primary" size="sm" className="me-2" onClick={() => enviarDatos(item)}>Edit</Button>
+                                    <Button color="danger" size="sm" onClick={() => eliminarProducto(item.id)}>Delete</Button>
+                                </td>
+                            </tr>
+                        ))
+                    )
+                }
+            </tbody>
+        </Table>
+    );
 }
 
 ProductsComponent.propTypes = {
-    data: PropTypes.func.isRequired,
-    mostrarModal: PropTypes.func.isRequired,
-    setMostrarModal: PropTypes.func.isRequired
+    data: propTypes.func.isRequired,
+    mostrarModal: propTypes.func.isRequired,
+    setMostrarModal: propTypes.func.isRequired,
+    setEditar: propTypes.func.isRequired,
+    eliminarProducto: propTypes.func.isRequired
 };
 
 export default ProductsComponent;
