c0f9acacd0b74c5cfb1b3d5328972424fe8fc3f4
Fagudelo <ferney.certifications@outlook.com>
2024-09-13 23:42:13 -0500

    Se crean los métodos 'editarProducto' y 'eliminarProducto' en el archivo 'App.jsx'

--- petshopapp.client/src/App.jsx
+++ petshopapp.client/src/App.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { Container, Row, Col, Card, CardHeader, CardBody, Button, CardTitle } from 'reactstrap';
-import ProductsComponent from './components/ProductsComponent'
-import './App.css';
+import ProductsComponent from './components/ProductsComponent';
 import ModalComponent from './components/ModalComponent';
+import './App.css';
 
 function App() {
 
     const [productos, setProductos] = useState([])
     const [mostrarModal, setMostrarModal] = useState(false)
+    const [editar, setEditar] = useState(null)
 
     const mostrarProductos = async () => {
         const response = await fetch("api/product/List");
@@ -39,6 +40,38 @@ function App() {
         }
     }
 
+    const editarProducto = async (product) => {
+        const response = await fetch("api/product/Update", {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(product)
+        });
+
+        if (response.ok) {
+            setMostrarModal(!mostrarModal)
+            mostrarProductos();
+        }
+    }
+
+    const eliminarProducto = async (id) => {
+
+        var respuesta = window.confirm("Esta seguro que desea eliminar el contacto?");
+
+        if (!respuesta) {
+            return;
+        }
+
+        const response = await fetch("api/product/Delete/" + id, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            mostrarProductos();
+        }
+    }
+
     return (
         <>
             <Container>
@@ -56,7 +89,9 @@ function App() {
                                 <ProductsComponent
                                     data={productos}
                                     mostrarModal={mostrarModal}
-                                    setMostrarModal={setMostrarModal}                               >
+                                    setMostrarModal={setMostrarModal}
+                                    setEditar={setEditar}
+                                    eliminarProducto={eliminarProducto}>
                                 </ProductsComponent>
                             </CardBody>
                         </Card>
@@ -65,7 +100,10 @@ function App() {
                 <ModalComponent
                     mostrarModal={mostrarModal}
                     setMostrarModal={setMostrarModal}
-                    guardarProducto={guardarProducto}>
+                    guardarProducto={guardarProducto}
+                    editar={editar}
+                    setEditar={setEditar}
+                    editarProducto={editarProducto}>
                 </ModalComponent>
             </Container>
         </>
