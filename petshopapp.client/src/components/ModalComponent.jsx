176305c5068e99da30edfe631c04d2358d0a7fe1
Fagudelo <ferney.certifications@outlook.com>
2024-09-13 20:32:25 -0500

    Se crean las funciones y el modal con el formulario para el ingreso de los productos

--- petshopapp.client/src/components/ModalComponent.jsx
+++ petshopapp.client/src/components/ModalComponent.jsx
@@ -1,7 +1,76 @@
-function ModalComponent() {
+import { Modal, Form, FormGroup, Label, Input, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import propTypes from 'prop-types';
+import { useState } from 'react';
+
+const modeloProducto = {
+    id: 0,
+    name: "",
+    category: "",
+    price: "",
+    quantityInStock: ""
+}
+function ModalComponent({ mostrarModal, setMostrarModal, guardarProducto }) {
+
+    const [producto, setProducto] = useState(modeloProducto)
+
+    const actualizarDato = (e) => {
+        console.log(e.target.name + ":" + e.target.value);
+        setProducto(
+            {
+                ...producto, [e.target.name]: e.target.value
+            }
+        )
+    }
+
+    const enviarDatos = () => {
+        if (producto.id == 0) {
+            guardarProducto(producto);
+        }
+        setProducto(modeloProducto)
+    }
+
+    const cerrarModal = () => {
+        setMostrarModal(!mostrarModal)
+    }
+
   return (
-    <p>Hello world!</p>
+      <Modal isOpen={mostrarModal}>
+          <ModalHeader>
+            
+          </ModalHeader>
+          <ModalBody>
+              <Form>
+                  <FormGroup>
+                      <Label>Name</Label>
+                      <Input name="name" onChange={(e) => actualizarDato(e)} value={ producto.name }></Input>
+                  </FormGroup>
+                  <FormGroup>
+                      <Label>Category</Label>
+                      <Input name="category" onChange={(e) => actualizarDato(e)} value={producto.category}></Input>
+                  </FormGroup>
+                  <FormGroup>
+                      <Label>Price</Label>
+                      <Input name="price" onChange={(e) => actualizarDato(e)} value={producto.price}></Input>
+                  </FormGroup>
+                  <FormGroup>
+                      <Label>Quantity In Stoke</Label>
+                      <Input name="quantityInStock" onChange={(e) => actualizarDato(e)} value={producto.quantityInStock}></Input>
+                  </FormGroup>
+              </Form>
+          </ModalBody>
+          <ModalFooter>
+              <Button color="primary" size="sm" onClick={enviarDatos}>Guardar</Button>
+              <Button color="danger" size="sm" onClick={cerrarModal}>Cerrar</Button>
+          </ModalFooter>
+      </Modal>
   );
+}
+
+ModalComponent.propTypes = {
+    mostrarModal: propTypes.func.isRequired,
+    setMostrarModal: propTypes.func.isRequired,
+    guardarProducto: propTypes.func
+
 }
 
 export default ModalComponent;
