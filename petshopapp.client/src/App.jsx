b22395e429e9487b51345d23c22c89b1e3b72e02
Fagudelo <ferney.certifications@outlook.com>
2024-09-13 20:24:51 -0500

    Se crea el método para agregar un nuevo producto en el archivo 'App.jsx'

--- petshopapp.client/src/App.jsx
+++ petshopapp.client/src/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import { Container, Row, Col, Card, CardHeader, CardBody, Button, CardTitle } from 'reactstrap';
 import ProductsComponent from './components/ProductsComponent'
 import './App.css';
+import ModalComponent from './components/ModalComponent';
 
 function App() {
 
     const [productos, setProductos] = useState([])
+    const [mostrarModal, setMostrarModal] = useState(false)
 
     const mostrarProductos = async () => {
         const response = await fetch("api/product/List");
@@ -22,6 +24,21 @@ function App() {
         mostrarProductos();
     }, [])
 
+    const guardarProducto = async (product) => {
+        const response = await fetch("api/product/Add", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(product)
+        });
+
+        if (response.ok) {
+            setMostrarModal(!mostrarModal)
+            mostrarProductos();
+        }
+    }
+
     return (
         <>
             <Container>
@@ -31,17 +48,25 @@ function App() {
                             <CardHeader>
                                 <CardTitle tag="h5">
                                     Lista de productos
-                                </CardTitle>                                
+                                </CardTitle>
                             </CardHeader>
                             <CardBody>
-                                <Button size="sm" color="success" >Nuevo Producto</Button>{' '}
+                                <Button size="sm" color="success" onClick={(() => setMostrarModal(!mostrarModal))}>Nuevo Producto</Button>{' '}
                                 <hr></hr>
-                                <ProductsComponent data={ productos }>
+                                <ProductsComponent
+                                    data={productos}
+                                    mostrarModal={mostrarModal}
+                                    setMostrarModal={setMostrarModal}                               >
                                 </ProductsComponent>
                             </CardBody>
                         </Card>
                     </Col>
                 </Row>
+                <ModalComponent
+                    mostrarModal={mostrarModal}
+                    setMostrarModal={setMostrarModal}
+                    guardarProducto={guardarProducto}>
+                </ModalComponent>
             </Container>
         </>
     );
